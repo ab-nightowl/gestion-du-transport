37438581afdea27c0e7cb5abe75749589de34c4e
Rossi Oddet <rossi.oddet@dev-institut.fr>
2017-09-04 22:27:41 +0200

    integration continue : déploiement Clever Cloud

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,8 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const API_URL = process.env.NODE_ENV === 'production' ? 'https://TODO_API' : 'http://localhost:8080';
-const publicPath = process.env.NODE_ENV === 'production' ? '' : '/TODO_CONTEXT/';
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://api-transport.cleverapps.io' : 'http://localhost:8080';
+const publicPath = process.env.NODE_ENV === 'production' ? '' : '/gestion-du-transport/';
 
 const output = 'public';
 
