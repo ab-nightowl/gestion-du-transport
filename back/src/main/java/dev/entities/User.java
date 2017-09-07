deadbbfabdfd5bf18e5aba6251ff247877a70e62
Lam Tan <nhat.tan93@yahoo.com>
2017-09-07 17:46:44 +0200

    authentif en cours - modal

--- back/src/main/java/dev/entities/User.java
+++ back/src/main/java/dev/entities/User.java
@@ -9,9 +9,18 @@ import javax.persistence.Id;
 public class User {
 	@Id
 	private String registrationNumber;
+	
+	private String nom;
+	private String prenom;
+	private String email;
+	private String password;
+	
+	
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
+	
+	
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
@@ -20,8 +29,60 @@ public class User {
 		return role;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+
 	public void setRole(Role role) {
 		this.role = role;
 	}
 
+	
+	public User(){
+		
+	}
+
+	public User(String registrationNumber, String nom, String prenom, String email, String password, Role role) {
+		super();
+		this.registrationNumber = registrationNumber;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+	
+	
 }
--- front/app/modules/tan.module.js
+++ front/app/modules/tan.module.js
@@ -1,26 +1,40 @@
 import collabBookVehicleSocietyComponent from '../components/collab.book.vehicle.society.component'
+import authentificationComponent from '../components/authentification.component'
 
 import bookVehicleService from '../services/book.vehicle.service'
 import bookVehicleController from '../controllers/book.vehicle.controller'
+
+import authentificationService from '../services/authentification.service'
+import authentificationController from '../controllers/authentification.controller'
+
+
 import urlsService from '../services/urls.service'
 import UiBootrap from 'angular-ui-bootstrap'
+
+import ngSessionStorage from 'angular-sessionstorage'
 // import momentPicker from 'angular-moment-picker'
 // import moment from 'moment'
 
-const tanModule = angular.module('tanModule',[UiBootrap])
+const tanModule = angular.module('tanModule',[UiBootrap,ngSessionStorage])
+.constant('urlsService', urlsService)
 
+.component('collabBookVehicleSocietyComponent', collabBookVehicleSocietyComponent)
 .controller('bookVehicleController', bookVehicleController)
 .service('bookVehicleService', bookVehicleService)
-.constant('urlsService', urlsService)
-// <rag-user-create></rag-user-create>
-.component('collabBookVehicleSocietyComponent', collabBookVehicleSocietyComponent)
 
+.component('authentificationComponent', authentificationComponent)
+.controller('authentificationController', authentificationController)
+.service('authentificationService', authentificationService)
 
 .config(($routeProvider)=>{
 	$routeProvider
 
 	.when('/collab/book/create',{
 		template : '<collab-book-vehicle-society-component></collab-book-vehicle-society-component>',
+	})
+
+	.when('/connexion',{
+		template : '<authentification-component></authentification-component>',
 	})
 })
 
