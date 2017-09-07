7e1f47af6ac384dddd5edf773f22427ab0759bdf
lamtan@ <lamtan93@gmail.com>
2017-09-07 02:10:32 +0200

    mettre en place mockito for test api rest

--- back/src/test/java/dev/vehicleRepositoryTest.java
+++ back/src/test/java/dev/vehicleRepositoryTest.java
@@ -4,6 +4,8 @@ package dev;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,12 +19,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import dev.entities.Vehicle;
 import dev.entities.VehicleStatus;
 import dev.repository.VehicleRepository;
+import dev.rest.VehicleController;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -32,8 +40,14 @@ public class vehicleRepositoryTest {
 	}
 	
 	@Autowired
-	VehicleRepository vehicleRepo;
+	private VehicleRepository vehicleRepo;
 	
+	
+//	@Autowired
+//	private MockMvc mockMvc;
+	
+//	@MockBean
+//    private VehicleController vehicleController;
 	
 	@org.junit.Before
 	public void setup(){
@@ -45,7 +59,7 @@ public class vehicleRepositoryTest {
 		);
 		
 		listV.forEach(v -> vehicleRepo.save(v));	
-	
+		
 	}
 	
 	
@@ -55,5 +69,14 @@ public class vehicleRepositoryTest {
 		List<Vehicle> listVehicles = vehicleRepo.findAll();		
 		assertThat(listVehicles.size()).isEqualTo(4);
 	}
+	
+	
+//	@Test
+//	public void getAllVehicles() throws Exception throws Exception {
+//        given(this.vehicleController.listVehicles())
+//        .willReturn(new VehicleDetails("Honda", "Civic"));
+//        this.mvc.perform(get("/sboot/vehicle").accept(MediaType.TEXT_PLAIN))
+//        .andExpect(status().isOk()).andExpect(content().string("Honda Civic"));
+//}
 	
 }
