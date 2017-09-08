cfd4aa2fc571db879c0b4cd2fdad19c2f397f10f
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-09-08 14:01:16 +0200

    Change

--- back/src/main/java/dev/rest/AdvertController.java
+++ back/src/main/java/dev/rest/AdvertController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,6 @@ import dev.repository.AdvertRepository;
 import dev.repository.UserRepository;
 import dev.services.AdvertService;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("/advert")
 public class AdvertController {
